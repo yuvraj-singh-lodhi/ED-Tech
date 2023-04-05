3d23d16dd706df82809bd730c93f25244b19c622
yuvraj <it2005@global.org.in>
2023-04-05 22:56:51 +0530

    initial commit

---
+++ Data/mainDB.js
@@ -0,0 +1,129 @@
+export const Tutorial ={
+    name :"main",
+    HTMLandCSS :{//this is an object 
+        // inside this i will create another object
+        name: "HTMLandCSS",
+        menu :[//arry
+            {
+                // arry ka object
+                title: "Learn HTML",
+                link : "https://www.w3schools.com/html/default.asp"
+            },
+            {
+                // arry ka object
+                title: "Learn CSS",
+                link :  "https://www.w3schools.com/css/default.asp"
+            },
+            {
+                // arry ka object
+                title: "Learn Bootstrap",
+                link :  "https://www.w3schools.com/bootstrap/bootstrap_ver.asp"
+            },
+            {
+                // arry ka object
+                title: "Learn RWD",
+                link :  "https://www.w3schools.com/css/css_rwd_intro.asp"
+            },
+            {
+                // arry ka object
+                title: "Learn Colors",
+                link :  "https://www.w3schools.com/colors/default.asp"
+            },
+            {
+                // arry ka object
+                title: "Learn icon",
+                link :  "https://www.w3schools.com/icons/default.asp"
+            },
+        ]
+
+    },
+    JavaScript:{
+        name:"javascript",
+        menu:[
+            {
+            title: "Learn javascript",
+            link :  "https://www.w3schools.com/js/default.asp"
+            },
+            {
+                title: "Learn jQuery",
+                link :  "https://www.w3schools.com/jquery/default.asp"
+            },
+            {
+                title: "Learn React",
+                link :  "https://www.w3schools.com/react/default.asp"
+            },
+            {
+                title: "Learn AngularJS",
+                link :  "https://www.w3schools.com/angular/default.asp"
+            },
+            {
+                title: "Learn JSON",
+                link :  "https://www.w3schools.com/js/js_json_intro.asp"
+            },
+            {
+                title: "Learn AJAX",
+                link :  "https://www.w3schools.com/js/js_ajax_intro.asp"
+            },
+
+        ]
+    },
+    ServerSide:{
+        name:"Server Side",
+        menu:[
+            {
+                title: "Learn SQL",
+                link :  "https://www.w3schools.com/sql/default.asp"
+            },
+            {
+                title: "Learn MySQL",
+                link :  "https://www.w3schools.com/mysql/default.asp"
+            },
+            {
+                title: "Learn PHP",
+                link :  "https://www.w3schools.com/php/default.asp"
+            },
+            {
+                title: "Learn ASP",
+                link :  "https://www.w3schools.com/asp/default.asp"
+            },
+            {
+                title: "Learn Node.js",
+                link :  "https://www.w3schools.com/nodejs/default.asp"
+            },
+            {
+                title: "Learn Raspberry Pi",
+                link :  "https://www.w3schools.com/nodejs/nodejs_raspberrypi.asp"
+            },
+        ]
+    },
+    DataAnalytics:{
+        name:"Data Analytics",
+        menu:[
+            {
+                title: "Learn AI",
+                link :  "https://www.w3schools.com/ai/default.asp"
+            },
+            {
+                title: "Learn ML",
+                link :  "https://www.w3schools.com/python/python_ml_getting_started.asp"
+            },
+            {
+                title: "Learn Data Science",
+                link :  "https://www.w3schools.com/datascience/default.asp"
+            },
+            {
+                title: "Learn NumPy",
+                link :  "https://www.w3schools.com/python/numpy/default.asp"
+            },
+            {
+                title: "Learn Pandas",
+                link :  "https://www.w3schools.com/python/pandas/default.asp"
+            },
+            {
+                title: "Learn SciPy",
+                link :  "https://www.w3schools.com/python/scipy/index.php"
+            },
+        ]
+    }
+
+}
---
+++ main.js
@@ -0,0 +1,105 @@
+import { Tutorial } from "./Data/mainDB.js"
+
+
+// section mobile btn
+
+let MenuBtnMobileEl= document.getElementById("menu-btn-mobile")
+let MenuBtnMobileElOpenBoolen =false
+let MobileMenuNavEl = document.getElementById("mobile-menu-nav")
+
+
+MenuBtnMobileEl.addEventListener("click",()=>{
+    if (MenuBtnMobileElOpenBoolen){
+        // MobileMenuNavEl.style.display="none"
+        MobileMenuNavEl.classList.remove("mobile-menu-nav-hidden")
+        MenuBtnMobileElOpenBoolen= false
+    }
+    else{
+        // MobileMenuNavEl.style.display ="block"
+        MobileMenuNavEl.classList.add("mobile-menu-nav-hidden")
+        MenuBtnMobileElOpenBoolen =true
+    }
+})
+
+let mobilenavcloseEl= document.getElementById("mobile-nav-close")
+    
+mobilenavcloseEl.addEventListener("click",()=>{
+    // MobileMenuNavEl.style.display="none"
+    MobileMenuNavEl.classList.remove("mobile-menu-nav-hidden")
+    MenuBtnMobileElOpenBoolen= false
+    })
+
+
+    // Tutorial-btn
+const NestedNavigationcloseBtn = document.getElementById("nested-navigation-close-btn")
+const NestedNavigationContainer = document.getElementById("nested-navigation-container_id")
+let TutorialEl = document.getElementById("Tutorial-btn")
+
+TutorialEl.addEventListener("click",toggleTutorial)
+NestedNavigationcloseBtn.addEventListener("click",toggleTutorial)
+
+function toggleTutorial(){
+    NestedNavigationContainer.classList.toggle("nested-navigation_hidden")
+}
+
+// tutorial data
+console.log(Tutorial)
+
+// 
+
+let NestedNavigationContainerContentID = document.getElementById("nested-navigation-container-contentID")
+let NestedNavigationHeading = ''
+let NestedNavigationTab1 = ''
+let NestedNavigationTab2 = ''
+let NestedNavigationTab3 = ''
+let NestedNavigationTab4 = ''
+//
+
+
+/*  heading*/
+NestedNavigationHeading += `<h1>${Tutorial.name}</h1>`
+
+
+//sub menu
+NestedNavigationTab1 +=`<h2>${Tutorial.HTMLandCSS.name}</h2>`
+Tutorial.HTMLandCSS.menu.forEach(el =>{
+    NestedNavigationTab1 += `<a href="${el.link}">${el.title}</a>`
+})
+
+
+NestedNavigationTab2 += `<h2>${Tutorial.JavaScript.name}</h2>`
+Tutorial.JavaScript.menu.forEach(el =>{
+    NestedNavigationTab2 += `<a href="${el.link}">${el.title}</a>`
+})
+
+NestedNavigationTab3 += `<h2>${Tutorial.ServerSide.name}</h2>`
+Tutorial.ServerSide.menu.forEach(el =>{
+    NestedNavigationTab3 += `<a href="${el.link}">${el.title}</a>`
+})
+
+NestedNavigationTab4 += `<h2>${Tutorial.DataAnalytics.name}</h2>`
+Tutorial.DataAnalytics.menu.forEach(el =>{
+    NestedNavigationTab4 += `<a href="${el.link}">${el.title}</a>`
+})
+
+
+
+//// render all the menu
+
+NestedNavigationContainerContentID.innerHTML=`
+    ${NestedNavigationHeading}
+     <div class="nested-navigation-container_content_data">
+            <div class="nested-navigation_item">
+                 ${NestedNavigationTab1}
+            </div>
+            <div class="nested-navigation_item">
+                 ${NestedNavigationTab2}
+            </div>
+            <div class="nested-navigation_item">
+                 ${NestedNavigationTab3}
+            </div>
+            <div class="nested-navigation_item">
+                 ${NestedNavigationTab4}
+            </div>
+    </div>
+`
